59e226f25e07eb06f904569eaa9868ab2fbfaeb1
aymen0810 <aymenrabehelbachi@gmail.com>
2025-07-19 23:37:28 +0200

    Commit poniendo eje,plos para luego buscar el patrón

--- 6 kyu/Arrays/wavesorting.js
+++ 6 kyu/Arrays/wavesorting.js
@@ -9,3 +9,35 @@ function waveSort(arr) {
 
     console.log(arr);
 }
+
+//EJEMPLOS:
+/**
+ * 
+ * function waveSort(arr) {
+  arr.sort((a,b) => b - a);
+  
+  for(let i = 0; i < Math.floor(arr.length / 2); i++) {
+    arr.splice((i *2 +1), 0, arr.pop());
+  }
+  
+
+}
+ * 
+ */
+
+/**
+ * 
+ * function ascending(a, b) {
+	return a - b;
+}
+
+function waveSort(arr) {
+  arr.sort(ascending);
+  for (var i = 0; i < arr.length - 1; i += 2) {
+    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+  }
+}
+ * 
+ */
+
+//BUSCAR EL PATRÓN
