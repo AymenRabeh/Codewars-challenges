84ee029af084926dbcbe217552d0138afaeec402
aymen0810 <aymenrabehelbachi@gmail.com>
2025-07-19 23:30:54 +0200

    Commit cambiando código

--- 6 kyu/Arrays/wavesorting.js
+++ 6 kyu/Arrays/wavesorting.js
@@ -1,17 +1,11 @@
 function waveSort(arr) {
+    arr.sort((a, b) => a - b);
 
-    let arrayWave = [];
+    for (let i = 0; i < arr.length - 1; i += 2) {
 
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i % 2 === 0] >= arr[(i + 1) % 2]){
-            arrayWave.push(arr[i]);
-        } else{
-            [arr[i], arr[(i + 1) % 2]] = [arr[(i + 1) % 2], arr[i]];
-            arrayWave.push(arr[i]);
-        }
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
 
     }
 
-
+    console.log(arr);
 }
